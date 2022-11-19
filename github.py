f88369a1ba5deb50d3e202bef356258e42264ea9
alisahin07 <alisahin172@gmail.com>
2022-11-20 01:28:22 +0300

    version2

---
+++ github.py
@@ -0,0 +1,7 @@
+'''
+git remote add origin https://github.com/alisahin07/onmuhasebe.git
+git add .
+git commit -m "version2"
+git branch -M version2
+git push -u origin version2
+'''
--- yelp.py
+++ yelp.py
@@ -1,16 +1,69 @@
-
+import eyaletler
+import requests
+import os
+import time
+import json
+import pandas    as pd
+import csv
 #Transaction Search   URL -- 'https://api.yelp.com/v3/transactions/{transaction_type}/search'
 #Autocomplete         URL -- 'https://api.yelp.com/v3/autocomplete'
 
 #Categories           URL -- 'https://api.yelp.com/v3/categories'
 #Categories Alias     URL -- 'https://api.yelp.com/v3/categories/{alias}'
+def CompanyAndPhone(xx):
+     text=''
+     for x in xx:
+         text=x['name']+';'+x['phone']
+         yazdir(text)
+
+
+
+
+def yazdir(yazi):
+    file1 = open(dosya_yolu, "a")
+    # \n is placed to indicate EOL (End of Line)
+    file1.write("  \n")
+    file1.writelines(yazi)
+    file1.close()  # to change file access modes
+    print(yazi)
+
+
+def sil(dosyaa):
+    if os.path.exists(dosyaa):  # FENER_KOLONLARI_OLUSTUR.sql dosyasi siliniyor
+        os.remove(dosyaa)
+    else:
+        print("The file does not exist")
+
+
+def convert_excell():
+    for x in eyaletler.eyalet():
+        try:
+            PARAMETERS = {'location': x
+
+                          }
+            response = requests.get(url=BUSINESS_PATH,
+                                    params=PARAMETERS,
+                                    headers=HEADERS)
+            time.sleep(1)
+            # Convert response to a JSON String
+            business_data = response.json()
+            time.sleep(1)
+            print(business_data['businesses'])
+            print(len(business_data['businesses']))
+            CompanyAndPhone(business_data['businesses'])
+        except:
+            print("An exception occurred")
+
+
+
+
+
 
 # Import the modules
-import requests
-import json
+
 
 # Define a business ID
-category_alias = 'hotdogs'
+#category_alias = 'hotdogs'
 
 # Define API Key, Search Type, and header
 MY_API_KEY = 'ZOZfxsbgPRb8UGMmlrElq_sVx1G-SdsH0mu6zuGvrlGmo2t5fRuzEdHgcCwJA5ySNssn7Ws6vS44gDq9IVUg5yOzjJ1a8wm14CNYL4tljMoqs0zLWaJyG2MO_c9vY3Yx'
@@ -18,18 +71,13 @@ BUSINESS_PATH = 'https://api.yelp.com/v3/transactions/delivery/search'
 HEADERS = {'Authorization': 'bearer %s' % MY_API_KEY}
 
 # Define the Parameters of the search
-PARAMETERS = {'location':'NYC'}
 
+dosya_yolu = r"C:\Users\Ali\Desktop\OnMuhasebe\test.txt"
+sil(dosya_yolu)
+convert_excell()
 # Make a Request to the API, and return results
-response = requests.get(url=BUSINESS_PATH, 
-                        params=PARAMETERS, 
-                        headers=HEADERS)
 
-# Convert response to a JSON String
-business_data = response.json()  
-print(business_data['businesses'])
-for x in business_data['businesses']:
-    print(x['name']+'--->telefon= '+x['phone'])
+
 # print the data
 #print(json.dumps(business_data, indent = 3))
 
